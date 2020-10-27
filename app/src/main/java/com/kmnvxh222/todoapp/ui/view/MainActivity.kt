4a7dd23bad411fdaee851f135f81cbed973550ab
Maria <km.nvxh222@gmail.com>
2020-10-27 17:06:33 +0300

    [fix] MainActivity

--- app/src/main/java/com/kmnvxh222/todoapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/kmnvxh222/todoapp/ui/view/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.kmnvxh222.todoapp.ui.view
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kmnvxh222.todoapp.R
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
-class MainActivity : DaggerAppCompatActivity() {
-
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
-
+class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
 }
